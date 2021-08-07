2c0073448793b0aca4ccc5b90c344394466da3c9
Saqib Shakil <saqibshakil@gmail.com>
2021-08-07 22:16:35 +0500

    dummy

--- server.js
+++ server.js
@@ -7,109 +7,114 @@ const app = express()
 // Link to Database
 // ========================
 // Updates environment variables
-require('./dotenv')
+//require('./dotenv')
 
 // Replace process.env.DB_URL with your actual connection string
 const connectionString = process.env.DB_URL
 
 MongoClient.connect(connectionString, { useUnifiedTopology: true })
   .then(client => {
-    console.log('Connected to Database')
-    const db = client.db(process.env.DB_NAME)
-    const quotesCollection = db.collection('quotes')
+    createApp(client)
+  })
+  .catch(createApp({ db: () => ({ collection: () => { } }) }))
 
-    // ========================
-    // Middlewares
-    // ========================
-    app.set('view engine', 'ejs')
-    app.use(bodyParser.urlencoded({ extended: true }))
-    app.use(bodyParser.json())
-    app.use(express.static('public'))
+function createApp(client) {
+  console.log('Connected to Database')
+  const db = client.db(process.env.DB_NAME)
+  const quotesCollection = db.collection('quotes')
 
-    // ========================
-    // Routes
-    // ========================
-    app.get('/', (req, res) => {
-      db.collection('quotes').find().toArray()
-        .then(quotes => {
-          res.render('index.ejs', { quotes: quotes })
-        })
-        .catch(/* ... */)
-    })
+  // ========================
+  // Middlewares
+  // ========================
+  app.set('view engine', 'ejs')
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(bodyParser.json())
+  app.use(express.static('public'))
 
-    app.get('/quotes', (req, res) => {
-      db.collection('quotes').find().toArray().then(quotes => {
-        res.json({ quotes: quotes })
+  // ========================
+  // Routes
+  // ========================
+  app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray()
+      .then(quotes => {
+        res.render('index.ejs', { quotes: quotes })
       })
-    })
+      .catch(/* ... */)
+  })
 
-    app.post('/quotes', (req, res) => {
-      quotesCollection.insertOne(req.body)
-        .then(result => {
-          res.redirect('/')
-        })
-        .catch(error => console.error(error))
-    })
-
-    app.put('/quotes', (req, res) => {
-      quotesCollection.findOneAndUpdate(
-        { name: 'Yoda' },
-        {
-          $set: {
-            name: req.body.name,
-            quote: req.body.quote
-          }
-        },
-        {
-          upsert: true
-        }
-      )
-        .then(result => {
-          console.log(result)
-          res.json(result)
-        })
-        .catch(error => console.error(error))
-    })
-    app.put('/quotes/:attr/:value', (req, res) => {
-      quotesCollection.findOneAndUpdate(
-        { [req.params.attr]: req.params.value },
-        {
-          $set: {
-            name: req.body.name,
-            quote: req.body.quote
-          }
-        },
-        {
-          upsert: true
-        }
-      )
-        .then(result => {
-          console.log(result)
-          res.json(result)
-        })
-        .catch(error => console.error(error))
-    })
-
-    app.delete('/quotes', (req, res) => {
-      quotesCollection.deleteOne(
-        { name: req.body.name }
-      )
-        .then(result => {
-          if (result.deletedCount === 0) {
-            return res.json('No quote to delete')
-          }
-          res.json('Deleted Darth Vadar\'s quote')
-        })
-        .catch(error => console.error(error))
-    })
-
-    // ========================
-    // Listen
-    // ========================
-    const isProduction = process.env.NODE_ENV === 'production'
-    const port = isProduction ? process.env.PORT : 3000
-    app.listen(port, function () {
-      console.log(`listening on ${port}`)
+  app.get('/quotes', (req, res) => {
+    db.collection('quotes').find().toArray().then(quotes => {
+      res.json({ quotes: quotes })
     })
   })
-  .catch(console.error)
+
+  app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+  })
+
+  app.put('/quotes', (req, res) => {
+    quotesCollection.findOneAndUpdate(
+      { name: 'Yoda' },
+      {
+        $set: {
+          name: req.body.name,
+          quote: req.body.quote
+        }
+      },
+      {
+        upsert: true
+      }
+    )
+      .then(result => {
+        console.log(result)
+        res.json(result)
+      })
+      .catch(error => console.error(error))
+  })
+  app.put('/quotes/:attr/:value', (req, res) => {
+    quotesCollection.findOneAndUpdate(
+      { [req.params.attr]: req.params.value },
+      {
+        $set: {
+          name: req.body.name,
+          quote: req.body.quote
+        }
+      },
+      {
+        upsert: true
+      }
+    )
+      .then(result => {
+        console.log(result)
+        res.json(result)
+      })
+      .catch(error => console.error(error))
+  })
+
+  app.delete('/quotes', (req, res) => {
+    quotesCollection.deleteOne(
+      { name: req.body.name }
+    )
+      .then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No quote to delete')
+        }
+        res.json('Deleted Darth Vadar\'s quote')
+      })
+      .catch(error => console.error(error))
+  })
+
+  // ========================
+  // Listen
+  // ========================
+  const isProduction = process.env.NODE_ENV === 'production'
+  const port = isProduction ? process.env.PORT || 7500 : 3000
+  console.log(`Trying listening on ${port}`)
+  app.listen(port, function () {
+    console.log(`listening on ${port}`)
+  })
+}
